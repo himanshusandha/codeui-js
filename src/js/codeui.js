5942bd663146defb7c075ca6d0067440d30afbb2
himanshusandha <himanshupatelj007@gmail.com>
2020-11-11 21:13:34 +0530

    Started with styling

--- src/js/codeui.js
+++ src/js/codeui.js
@@ -1,9 +1,13 @@
 import React from "react";
+import "../style/codeui.css";
 
 function Codeui(props) {
   return (
-    <pre>
-      <code>
+    <pre
+      className="codeui-base"
+      contentEditable={props.contentEditable ? props.contentEditable : false}
+    >
+      <code className="codeui-code-tag">
         <div>{props.data}</div>
       </code>
     </pre>
